2adcacb770e8bf66110f9d13befe7165fc8908a1
AmadeoMB <amadeo.9b33@gmail.com>
2021-12-03 14:19:27 +0700

    exporting a package

--- lib/ct_plugins.dart
+++ lib/ct_plugins.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+export 'package:shimmer/shimmer.dart';
+
 class CtPlugins {
   static const MethodChannel _channel = MethodChannel('ct_plugins');
 
