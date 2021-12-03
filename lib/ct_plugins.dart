b2b5751aa45f52b1c41d00ce4854bedaad58bd17
AmadeoMB <amadeo.9b33@gmail.com>
2021-12-03 16:06:48 +0700

    Removing some package

--- lib/ct_plugins.dart
+++ lib/ct_plugins.dart
@@ -2,25 +2,17 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
 
-import 'package:get/get.dart' hide FormData, MultipartFile, Response;
-import 'package:get_storage/get_storage.dart';
-import 'package:shimmer/shimmer.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
-import 'package:flutter_html/flutter_html.dart' hide OnTap;
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:email_validator/email_validator.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:lottie/lottie.dart';
-import 'package:line_icons/line_icon.dart';
-import 'package:line_icons/line_icons.dart';
+export 'package:get/get.dart' hide FormData, MultipartFile, Response;
+export 'package:get_storage/get_storage.dart';
+export 'package:shimmer/shimmer.dart';
+export 'package:dio/dio.dart';
+export 'package:flutter_local_notifications/flutter_local_notifications.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:firebase_messaging/firebase_messaging.dart';
+export 'package:email_validator/email_validator.dart';
+export 'package:flutter_html/flutter_html.dart' hide OnTap;
+export 'package:cached_network_image/cached_network_image.dart';
 
 
 class CtPlugins {
