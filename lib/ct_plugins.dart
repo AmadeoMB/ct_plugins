a732b559b4752bd2e011346bf8e95106a6233bb8
AmadeoMB <amadeo.9b33@gmail.com>
2021-12-03 15:52:50 +0700

    change export to import

--- lib/ct_plugins.dart
+++ lib/ct_plugins.dart
@@ -2,25 +2,25 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
 
-export 'package:get/get.dart' hide FormData, MultipartFile, Response;
-export 'package:get_storage/get_storage.dart';
-export 'package:shimmer/shimmer.dart';
-export 'package:dio/dio.dart';
-export 'package:flutter_local_notifications/flutter_local_notifications.dart';
-export 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
-export 'package:flutter_typeahead/flutter_typeahead.dart';
-export 'package:flutter_html/flutter_html.dart' hide OnTap;
-export 'package:fluttertoast/fluttertoast.dart';
-export 'package:email_validator/email_validator.dart';
-export 'package:firebase_core/firebase_core.dart';
-export 'package:firebase_auth/firebase_auth.dart';
-export 'package:firebase_messaging/firebase_messaging.dart';
-export 'package:cloud_firestore/cloud_firestore.dart';
-export 'package:cached_network_image/cached_network_image.dart';
-export 'package:image_picker/image_picker.dart';
-export 'package:lottie/lottie.dart';
-export 'package:line_icons/line_icon.dart';
-export 'package:line_icons/line_icons.dart';
+import 'package:get/get.dart' hide FormData, MultipartFile, Response;
+import 'package:get_storage/get_storage.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:flutter_html/flutter_html.dart' hide OnTap;
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:lottie/lottie.dart';
+import 'package:line_icons/line_icon.dart';
+import 'package:line_icons/line_icons.dart';
 
 
 class CtPlugins {
